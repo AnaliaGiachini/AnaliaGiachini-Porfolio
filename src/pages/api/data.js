2e276cb4c56a244080349ed1f3f252dc510bfb2b
AnaliaGiachini <analiagiachini@gmail.com>
2023-09-03 19:51:52 +0200

    modification

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -3,14 +3,14 @@ export const projectData = [
     id: 1,
     title: 'Countries App',
     description:
-      'An application of Countries with JavaScript. It allows you to search for countries using their name. When selecting the country, information such as the name, its flag, its capital, its continent and other relevant data is displayed.',
+      'Development of a Web Application about countries around the world. Allows you to search for countries using their name. When selecting the country, information such as the name, its flag, its capital, its continent and other relevant data is displayed.You can create a tourist activity by country, it also contains orders and filters by different criteria such as alphabetically ascending or descending, by amount of population, by continents and tourist activities.',
     gitHubLink: 'https://github.com/AnaliaGiachini/PI-Countries-main',
   },
   {
     id: 2,
     title: 'Salvando Huellas App',
     description:
-      'Web Application for a Canine Shelter with JavaScript, in this application you can generate an adoption application for the different dogs that are in the shelter. You can view them through the different filters such as size, age and gender. In addition, the application has an e-commerce, a donation section, user records and administrator management, among other features.',
+      'Web Application for an Animal Shelter.In it, users can generate an adoption form for the available animals. You can search for them through the different filters such as size, age and gender. In addition, the application has electronic commerce, a donation section, user registration and administrator management, among other features.',
     gitHubLink: 'https://github.com/AnaliaGiachini/PF-Salvando-Huellas',
   },
 ];
