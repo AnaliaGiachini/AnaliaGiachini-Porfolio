0afb141d9ca76e1bf0e5497b62b4d657d688b03c
AnaliaGiachini <analiagiachini@gmail.com>
2023-09-17 15:30:26 +0200

    jumping

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,17 +7,14 @@ const About = () => {
       <div className="flex-about">
         <div className="about-text">
           <p>
-            I am a Software Developer with experience in creating web applications, both on the client and server sides. My skill set includes a strong knowledge base in technologies like JavaScript, React, and Node.js. I have gained expertise in the IT world by tackling various challenges and projects.
-          </p>
-          <p>
-
+            I am a Software Developer with experience in creating web applications, both on the client and server sides. My skill set includes a strong knowledge base in technologies like JavaScript, React, and Node.js. I have gained expertise in the IT world by tackling various challenges and projects.<br></br>
+         
+        
             I have had the opportunity to travel and experience diverse cultures, which has fostered my open and receptive mindset. I always seek to learn something new and productive in each experience, driving my continuous development.
-          </p>
-          <p>
+          
             I have collaborated on projects that required communication and cooperation, but I have also led initiatives autonomously, demonstrating self-discipline and self-management skills.
             My experience with agile methodologies like Scrum and my proficiency in Git and GitHub make me an efficient contributor to any development team.
-          </p>
-          <p>
+          
             In the future, I am looking for a job that challenges me and promotes my professional growth in IT. I wish to join a dynamic team where I can apply my technical skills to projects that have a positive impact on the community or the company.
           </p>
         </div>
