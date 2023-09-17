073a6becd00762d569d6803951cde0d3a2d90ac5
AnaliaGiachini <analiagiachini@gmail.com>
2023-09-17 12:38:17 +0200

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,11 @@ const About = () => {
             I am a Software Developer with experience in creating web applications, both on the client and server sides. My skill set includes a strong knowledge base in technologies like JavaScript, React, and Node.js. I have gained expertise in the IT world by tackling various challenges and projects. But my story goes beyond my professional career, as my life has been marked by constant adaptation to different scenarios, something I consider one of my greatest strengths.
 
             I have had the opportunity to travel and experience diverse cultures, which has fostered my open and receptive mindset. I always seek to learn something new and productive in each experience, driving my continuous development.
+            
             My versatility to work both in teams and independently reflects my adaptability to various work dynamics. I have collaborated on projects that required communication and cooperation, but I have also led initiatives autonomously, demonstrating self-discipline and self-management skills.
             My experience with agile methodologies like Scrum and my proficiency in Git and GitHub make me an efficient contributor to any development team.
 
             In the future, I am looking for a job that challenges me and promotes my professional growth in IT. I wish to join a dynamic team where I can apply my technical skills to projects that have a positive impact on the community or the company.
-
-            Furthermore, my dedication to learning languages, such as English, reflects my commitment to effective communication and global collaboration.
-            My discipline and focus, developed through my passions like yoga and soccer, are assets that will strengthen my work ethic in any future employment.
-
-            My unique combination of experience, skills, and knowledge makes me valuable in addressing the future workplace challenges. I look forward to continuing my professional journey and making a significant contribution in any role I take on.
           </p>
         </div>
         <div className="about-img">
